27975992e4c78f47e0cc26ec19762e7b5d82ebaf
saneness <saneness@protonmail.com>
2025-06-13 08:40:20 +0100

    remove order line

--- src/views/StudentProgressView.vue
+++ src/views/StudentProgressView.vue
@@ -38,7 +38,6 @@
           >
             <div>
               <h3 class="text-xl font-semibold text-gray-800">{{ lesson.topic }}</h3>
-              <p class="text-xs text-gray-400 mt-0.5">Order: {{ lesson.order === null ? 'N/A' : lesson.order }}</p>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500 transition-transform duration-300" 
                  :class="{'rotate-180': activeLessonId === lesson.id}" fill="none" viewBox="0 0 24 24" stroke="currentColor">
